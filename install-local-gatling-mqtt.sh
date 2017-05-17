bfafdce2c6249181259aeb278abca14aa6e32cdf
volodymyr-babak <volodymyr.babak@gmail.com>
2017-05-17 13:17:04 +0300

    Fixed groupId

--- install-local-gatling-mqtt.sh
+++ install-local-gatling-mqtt.sh
@@ -4,4 +4,4 @@ sbt assembly
 
 # install gatling mqtt jar file into local maven repository
 # these classes are used in thingsboard performance-tests project
-mvn install:install-file -Dfile=target/scala-2.11/gatling-mqtt-assembly-0.1.0-SNAPSHOT.jar -DgroupId=com.github.mnogu -DartifactId=gatling-mqtt -Dversion=1.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=target/scala-2.11/gatling-mqtt-assembly-0.1.0-SNAPSHOT.jar -DgroupId=org.thingsboard -DartifactId=gatling-mqtt -Dversion=1.0.0 -Dpackaging=jar
